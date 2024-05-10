218a3e23a2329a048f28e64ce0e49da7a6807bd4
andreas@bizfactory.tech <andreas@bizfactory.tech>
2024-05-10 10:21:45 +0200

    ZipUtil.java - check for path traversal attack

--- modules/common/app/utils/common/ZipUtil.java
+++ modules/common/app/utils/common/ZipUtil.java
@@ -11,7 +11,6 @@ import java.nio.file.Paths;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
